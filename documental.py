ba06af28b63365b2c8b0675262b6b2247c3b4787
mauriciogallo32 <mauriciogallo978@gmail.com>
2023-04-29 13:44:42 -0600

    agregamos documental.py

---
+++ documental.py
@@ -0,0 +1,15 @@
+#Informativo N°2 del documental
+dec=input("\nQuieres saber como se relaciona el Big data con Amazon y qué es cada uno?\nsi o no")
+if dec=="si":
+    print("Este programa está creado con el fin de educarte un poco en base al Big data que se presenta en Amazon\n")
+    input("\nprecione enter para continuar:")
+    print("¿Qué es el Big data?\n\nEl big data le permite recopilar datos de redes sociales, visitas a páginas web,\nregistros de llamadas y otras fuentes para mejorar la experiencia de interacción,\nasí como maximizar el valor ofrecido.")
+    input("\nprecione enter para continuar:")
+    print("\n¿Qué es Amazon?\n")
+    print("Es una tienda de comercio electrónico estadounidense que ofrece una amplia gama de productos, incluyendo\nlibros, música, películas, y computadoras. También ofrece servicios basados en la web,\nfabrica dispositivos electrónicos tales como Kindle y tabletas, y ofrece servicios de auto-publicación.")
+    input("\nprecione enter para continuar:")
+    print("\n\nEl uso de Big Data en Amazon está basado en el machine learning, es decir, la capacidad de los sistemas de\naprender a través del análisis de datos masivos que se obtiene de la recogida,\nalmacenamiento y ordenamiento que hace el Big Data.Así que cuando hablamos Amazon y el Big Data,\nen realidad nos estamos refiriendo a la aplicación de diferentes técnicas que emplean algoritmos para predecir el comportamiento de los usuarios y\nmejorar su experiencia en la plataforma.")
+    input("\nSe le felicita por querer adquirir este conocimiento, si deseas segir creciendo como persona siguenos en nuestro Instagram de crecimiento personal: @calidadnocantidad_")
+else: 
+    print("\n\nGracias por su tiempo si desea saber más aquí le comparto un link sobre Amazon.\nhttps://www.youtube.com/watch?v=UzGemfwaTT8")
+    input("")
